619a8736c13c13fbe03576f0bf9ba3efab04e7fe
Steve Loar <sjl7909@gmail.com>
2015-11-11 09:24:02 -0500

    Add Exercise 2 - high-order.js

---
+++ higher-order.js
@@ -0,0 +1,10 @@
+"use strict";
+function repeat(operation, num) {
+ 	if (num > 0) {
+ 		repeat(operation, --num)
+ 		operation
+ 	}
+}
+    
+	// Do not remove the line below
+  module.exports = repeat
