de841908cefcab485ee8c18c070caad6293e56b3
Steve Loar <sjl7909@gmail.com>
2015-11-11 15:16:32 -0500

    Add Exercise 8 - duckCount.js

---
+++ duckCount.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function duckCount() {
+	return Array.prototype.slice.call(arguments).filter(function(object){
+		return Object.prototype.hasOwnProperty.call(object, 'quack');
+	}).length;
+}
+
+module.exports = duckCount
